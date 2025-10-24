ee67605e7749da6054ff9c5fbb58e110dedbb7c7
98hub <akun.sumberweb@gmail.com>
2025-10-24 17:00:12 +0700

    fix deploy.yml (9)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "standalone",
   reactCompiler: true,
 };
+
+if (isProd) {
+  process.env.NEXT_USE_TURBOPACK = "0";
+  console.log("🧱 Using Webpack (Turbopack disabled for production build)");
+} else {
+  process.env.NEXT_USE_TURBOPACK = "1";
+  console.log("⚡ Using Turbopack for faster dev builds");
+}
 
 export default nextConfig;
